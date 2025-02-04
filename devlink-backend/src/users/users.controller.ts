4b698132529dc40281c9a6b9b3acad3d2e9dfcc8
ThanhNhax <thanhnha.k13@gmail.com>
2025-02-04 15:56:31 +0700

    chore: udpate httpStatus swagger view

--- devlink-backend/src/users/users.controller.ts
+++ devlink-backend/src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { ApiBody, ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -10,7 +20,7 @@ export class UsersController {
 
   @Get()
   @ApiOperation({ summary: 'Get all users with search & pagination' })
-  @ApiResponse({ status: 200, description: 'List of users with pagination' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'List of users with pagination' })
   @ApiQuery({ name: 'search', required: false }) // ✅ Làm `search` thành optional
   @ApiQuery({ name: 'page', required: false, example: 1 })
   @ApiQuery({ name: 'limit', required: false, example: 10 })
@@ -24,16 +34,16 @@ export class UsersController {
 
   @Get(':id')
   @ApiOperation({ summary: 'Get user by ID' })
-  @ApiResponse({ status: 200, description: 'User found' })
-  @ApiResponse({ status: 404, description: 'User not found' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'User found' })
+  @ApiResponse({ status: HttpStatus.NOT_FOUND, description: 'User not found' })
   getUserById(@Param('id') id: string) {
     return this.usersService.getUserById(id);
   }
 
   @Post()
   @ApiOperation({ summary: 'Create a new user' })
-  @ApiResponse({ status: 201, description: 'User created' })
-  @ApiResponse({ status: 400, description: 'Invalid input' })
+  @ApiResponse({ status: HttpStatus.CREATED, description: 'User created' })
+  @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Invalid input' })
   @ApiBody({ type: CreateUserDto })
   createUser(@Body() body: { name: string; email: string; age?: number }) {
     return this.usersService.createUser(body);
@@ -41,9 +51,9 @@ export class UsersController {
 
   @Patch(':id')
   @ApiOperation({ summary: 'Update user by ID' })
-  @ApiResponse({ status: 200, description: 'User updated' })
-  @ApiResponse({ status: 400, description: 'Invalid ID format or email taken' })
-  @ApiResponse({ status: 404, description: 'User not found' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'User updated' })
+  @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: 'Invalid ID format or email taken' })
+  @ApiResponse({ status: HttpStatus.NOT_FOUND, description: 'User not found' })
   @ApiBody({ type: UpdateUserDto })
   updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.updateUser(id, updateUserDto);
@@ -51,8 +61,8 @@ export class UsersController {
 
   @Delete(':id')
   @ApiOperation({ summary: 'Delete user by ID' })
-  @ApiResponse({ status: 204, description: 'User deleted' })
-  @ApiResponse({ status: 404, description: 'User not found' })
+  @ApiResponse({ status: HttpStatus.NO_CONTENT, description: 'User deleted' })
+  @ApiResponse({ status: HttpStatus.NOT_FOUND, description: 'User not found' })
   deleteUser(@Param('id') id: string) {
     return this.usersService.deleteUser(id);
   }
